503dee3ab99cedfb416806338cbff66ab303c361
Daniel <daniel.rezende@alunoescolassociais.org.br>
2024-08-08 08:11:18 -0300

    cria a tabela para ponhar o nome em lista

--- controller.js
+++ controller.js
@@ -1,10 +1,10 @@
-// VALIDAÇÃO DE PREEMCHIMENTO
+// VALIDAÇÃO DE PREENCHIMENTO
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
  
     if(!loginEmail || !loginSenha){
-        alert('Por favor, preencher todos os campos.');
+        alert('Favor preencher todos os campos');
     }else{
         //alert("Campos preenchidos com sucesso");
         window.location.href = 'cadastro.html';
@@ -13,24 +13,26 @@ function acessar(){
  
 // FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES
  
-var dadosLista = [];
+var dadosLista =[];
  
 function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
  
     if(nomeUser){
         dadosLista.push(nomeUser);
-       console.log(dadosLista);
-        document.getElementById('nomeUser').value ="";
-    }else{
-        alert("Favor, informar um nome");
+        // console.log(dadosLista);
+        criaLista();
+        document.getElementById('nomeUser').value = "";
+    } else{
+        alert("Favor, informar um nome para cadastro.");
     }
 }
-
+ 
 // FUNÇÃO DE CRIAÇÃO DE LISTA
-function crialista(){
-    let tabela = docuet.getElementbyid('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-    for(let i = 0; i <= (dadoslista.length - 1); i++){
-        tabela += '<tr></th>' = dadosLista[i] + "</td><td></td></tr>";
+function criaLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for(let i = 0; i <= (dadosLista.length - 1); i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
